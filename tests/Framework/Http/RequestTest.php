4a18064cc801f9ef4e3e7c991e4a40f78f9b3a3b
pavel02 <pavel.hr@mail.ru>
2018-10-17 20:07:24 +0300

    Added request tests

---
+++ tests/Framework/Http/RequestTest.php
@@ -0,0 +1,53 @@
+<?php
+
+use Framework\Http\Request;
+use PHPUnit\Framework\TestCase;
+
+class RequestTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        $_GET = [];
+        $_POST = [];
+    }
+
+    /**
+     * Проверяем, что при заданных пустых GET и POST
+     * методы возвращают ожидаемые значения
+     */
+    public function testEmpty(): void
+    {
+         $_GET = [];
+         $_POST = [];
+
+         $request = new Request();
+
+         self::assertEquals([], $request->getQueryParams());
+         self::assertNull($request->getParsedBody());
+    }
+
+    public function testQueryParams(): void
+    {
+        $_GET = $data = [
+            'name' => 'John',
+            'age' => 28
+        ];
+
+        $request = new Request();
+
+        self::assertEquals($data, $request->getQueryParams());
+        self::assertNull($request->getParsedBody());
+    }
+
+    public function testParsedBody(): void
+    {
+        $_POST = $data = ['title' => 'Title'];
+
+        $request = new Request();
+
+        self::assertEquals([], $request->getQueryParams());
+        self::assertEquals($data, $request->getParsedBody());
+    }
+}
