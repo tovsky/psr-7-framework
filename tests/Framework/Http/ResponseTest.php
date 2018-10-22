ba57d9b01798b0bb31bde8523674f777d6907380
pavel02 <pavel.hr@mail.ru>
2018-10-22 19:47:56 +0300

    Added PSR7 interface

--- tests/Framework/Http/ResponseTest.php
+++ tests/Framework/Http/ResponseTest.php
@@ -1,5 +1,7 @@
 <?php
 
+namespace Tests\Framework\Http;
+
 use Framework\Http\Response;
 use PHPUnit\Framework\TestCase;
 
@@ -9,7 +11,7 @@ class ResponseTest extends TestCase
     {
         $response = new Response($body = 'Body');
 
-        self::assertEquals($body, $response->getBody());
+        self::assertEquals($body, $response->getBody()->getContents());
         self::assertEquals(200, $response->getStatusCode());
         self::assertEquals('OK', $response->getReasonPhrase());
     }
@@ -18,7 +20,8 @@ class ResponseTest extends TestCase
     {
         $response = new Response($body = 'Empty', $status = 404);
 
-        self::assertEquals($body, $response->getBody());
+        self::assertEquals($body, $response->getBody()->getContents());
+        self::assertEquals(mb_strlen($body), $response->getBody()->getSize());
         self::assertEquals($status, $response->getStatusCode());
         self::assertEquals('Not Found', $response->getReasonPhrase());
     }
@@ -29,6 +32,6 @@ class ResponseTest extends TestCase
             ->withHeader($name1 = 'X-Header-1', $value1 = 'value_1')
             ->withHeader($name2 = 'X-Header-2', $value2 = 'value_2');
 
-        self::assertEquals([$name1 => $value1, $name2 => $value2], $response->getHeaders());
+        self::assertEquals([$name1 => [$value1], $name2 => [$value2]], $response->getHeaders());
     }
 }
