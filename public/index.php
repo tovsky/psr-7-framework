146687e6a99fed93bceceae0caca7e336a67611f
pavel02 <pavel.hr@mail.ru>
2018-10-30 20:00:06 +0300

    Added blog actions

--- public/index.php
+++ public/index.php
@@ -1,6 +1,7 @@
 <?php
 
 use Zend\Diactoros\Response\HtmlResponse;
+use Zend\Diactoros\Response\JsonResponse;
 use Zend\HttpHandlerRunner\Emitter\SapiEmitter;
 use Zend\Diactoros\ServerRequestFactory;
 
@@ -19,6 +20,18 @@ if ($path === '/') {
     $response = new HtmlResponse('Hello, ' . $name . '!');
 } elseif ($path === '/about') {
     $response = new HtmlResponse('I am a simple site');
+} elseif ($path === '/blog') {
+    $response = new JsonResponse([
+        ['id' => 2, 'title' => 'The Second Post'],
+        ['id' => 1, 'title' => 'The First Post'],
+    ]);
+} elseif (preg_match('#^/blog/(?P<id>\d+)$#i', $path, $matches)) {
+    $id = $matches['id'];
+    if ($id > 2) {
+        $response = new JsonResponse(['error' => 'Undefined page'], 404);
+    } else {
+        $response = new JsonResponse(['id' => $id, 'title' => 'Post #' . $id]);
+    }
 } else {
     $response = new HtmlResponse('Undefined page', 404);
 }
