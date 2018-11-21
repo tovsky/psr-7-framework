967849fd161957c9a0d33a7ccebb369495d7c995
pavel02 <pavel.hr@mail.ru>
2018-11-21 15:53:28 +0300

    Extracted router interface

--- src/Framework/Http/Router/Router.php
+++ src/Framework/Http/Router/Router.php
@@ -2,50 +2,25 @@
 
 namespace Framework\Http\Router;
 
+
 use Framework\Http\Router\Exception\RequestNotMatchedException;
 use Framework\Http\Router\Exception\RouteNotFoundException;
 use Psr\Http\Message\ServerRequestInterface;
 
-class Router
+interface Router
 {
-    private $routes;
-
     /**
-     * сохраняем всю коллекцию роутеров внутри объекта
-     *
-     * Router constructor.
-     * @param RouteCollection $routes
-     */
-    public function __construct(RouteCollection $routes)
-    {
-        $this->routes = $routes;
-    }
-
-    /**
-     * Парсим запрос и ищем совпадение с имеющимися маршрутами
-     *
      * @param ServerRequestInterface $request
+     * @throws RequestNotMatchedException
      * @return Result
      */
-    public function match(ServerRequestInterface $request): Result
-    {
-        foreach ($this->routes->getRoutes() as $route) {
-            if ($result = $route->match($request)) {
-                return $result;
-            }
-        }
+    public function match(ServerRequestInterface $request): Result;
 
-        throw new RequestNotMatchedException($request);
-    }
-
-    public function generate($name, array $params = []): string
-    {
-        foreach ($this->routes->getRoutes() as $route) {
-            if (null !== $url = $route->generate($name, array_filter($params))) {
-                return $url;
-            }
-        }
-
-        throw new RouteNotFoundException($name, $params);
-    }
+    /**
+     * @param $name
+     * @param array $params
+     * @throws RouteNotFoundException
+     * @return string
+     */
+    public function generate($name, array $params): string;
 }
